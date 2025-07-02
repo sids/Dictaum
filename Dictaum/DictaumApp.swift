d689838908695a013582c211b6e4070761d6db18
Siddhartha Reddy <sids.reddy@gmail.com>
2025-07-02 14:43:06 +0530

    Initial Commit

---
+++ Dictaum/DictaumApp.swift
@@ -0,0 +1,17 @@
+//
+//  DictaumApp.swift
+//  Dictaum
+//
+//  Created by Siddhartha Reddy on 02/07/25.
+//
+
+import SwiftUI
+
+@main
+struct DictaumApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ DictaumTests/DictaumTests.swift
@@ -0,0 +1,17 @@
+//
+//  DictaumTests.swift
+//  DictaumTests
+//
+//  Created by Siddhartha Reddy on 02/07/25.
+//
+
+import Testing
+@testable import Dictaum
+
+struct DictaumTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
